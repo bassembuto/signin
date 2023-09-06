7200252493a6ca7bc86823129b84e339a6c3891d
bassembuto <132224648+bassembuto@users.noreply.github.com>
2023-09-06 21:32:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let pro1 = prompt('من فضلك أكتب الأيميل');
+let pro2 = prompt('من فضلك أكتب كلمة المرور');
+
+let email = 'bassemshanlo@gmail.com';
+let password = 12345;
+
+console.log(pro1.trim().toLowerCase() == email && pro2 == password);
+
+
+if (pro1 == email && pro2 == password) {
+    document.write('<a href = "link.html">دخول</a>')
+} else {
+    document.write('<a href = "index.html">هناك خطأ في كلمة المرور أو الأيميل من فضلك أعد المحاولة.</a>')
+}
+
