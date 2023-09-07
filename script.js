7dd3c098b35e38730e212c20023654e55549fd9c
bassembuto <132224648+bassembuto@users.noreply.github.com>
2023-09-07 13:44:26 +0300

    script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let pro2 = prompt('من فضلك أكتب كلمة المرور');
 let email = 'bassemshanlo@gmail.com';
 let password = 12345;
 
-console.log(pro1.toLowerCase() == email && pro2 == password);
+console.log(pro1.toLowerCase().trim() == email && pro2 == password);
 
 
-if (pro1 == email && pro2 == password) {
+if (pro1.toLowerCase().trim() == email && pro2 == password) {
     document.write('<a href = "link.html">دخول</a>')
 } else {
     document.write('<a href = "index.html">هناك خطأ في كلمة المرور أو الأيميل من فضلك أعد المحاولة.</a>')
